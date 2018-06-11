a4fb3fa91c8aeb6064aa705517f5601cc90c63e3
SSIMAKATI\adulay <allandulay69@gmail.com>
2018-06-11 16:14:37 +0800

    Update

--- rainCheck/rainCheck/Form_Landing.Designer.cs
+++ rainCheck/rainCheck/Form_Landing.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Landing));
             this.panel_authorization = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label_isp = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label_country = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label_region = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label_city = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label_login = new System.Windows.Forms.Label();
             this.label_logo_login = new System.Windows.Forms.Label();
@@ -38,22 +46,30 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.pictureBox_loader = new System.Windows.Forms.PictureBox();
-            this.panel_retry_authorisation = new System.Windows.Forms.Panel();
-            this.button_retry_authorisation = new System.Windows.Forms.Button();
-            this.label_noconnection = new System.Windows.Forms.Label();
-            this.panel_retry_main = new System.Windows.Forms.Panel();
-            this.button_retry_main = new System.Windows.Forms.Button();
+            this.panel_retry = new System.Windows.Forms.Panel();
+            this.button_retry = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label_ip = new System.Windows.Forms.Label();
             this.panel_authorization.SuspendLayout();
             this.panel_loader.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_loader)).BeginInit();
-            this.panel_retry_authorisation.SuspendLayout();
-            this.panel_retry_main.SuspendLayout();
+            this.panel_retry.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel_authorization
             // 
-            this.panel_authorization.Controls.Add(this.button1);
+            this.panel_authorization.Controls.Add(this.label_ip);
+            this.panel_authorization.Controls.Add(this.label6);
+            this.panel_authorization.Controls.Add(this.label_isp);
+            this.panel_authorization.Controls.Add(this.label11);
+            this.panel_authorization.Controls.Add(this.label_country);
+            this.panel_authorization.Controls.Add(this.label9);
+            this.panel_authorization.Controls.Add(this.label_region);
+            this.panel_authorization.Controls.Add(this.label7);
+            this.panel_authorization.Controls.Add(this.label_city);
+            this.panel_authorization.Controls.Add(this.label5);
             this.panel_authorization.Controls.Add(this.label2);
             this.panel_authorization.Controls.Add(this.label_login);
             this.panel_authorization.Controls.Add(this.label_logo_login);
@@ -62,22 +78,84 @@
             this.panel_authorization.Size = new System.Drawing.Size(680, 457);
             this.panel_authorization.TabIndex = 14;
             // 
-            // button1
+            // label_isp
             // 
-            this.button1.Location = new System.Drawing.Point(308, 71);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 15;
-            this.button1.Text = "test";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label_isp.AutoSize = true;
+            this.label_isp.Location = new System.Drawing.Point(74, 174);
+            this.label_isp.Name = "label_isp";
+            this.label_isp.Size = new System.Drawing.Size(13, 13);
+            this.label_isp.TabIndex = 22;
+            this.label_isp.Text = "4";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(27, 174);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(20, 13);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "isp";
+            // 
+            // label_country
+            // 
+            this.label_country.AutoSize = true;
+            this.label_country.Location = new System.Drawing.Point(74, 143);
+            this.label_country.Name = "label_country";
+            this.label_country.Size = new System.Drawing.Size(13, 13);
+            this.label_country.TabIndex = 20;
+            this.label_country.Text = "3";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(27, 143);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(42, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "country";
+            // 
+            // label_region
+            // 
+            this.label_region.AutoSize = true;
+            this.label_region.Location = new System.Drawing.Point(74, 112);
+            this.label_region.Name = "label_region";
+            this.label_region.Size = new System.Drawing.Size(13, 13);
+            this.label_region.TabIndex = 18;
+            this.label_region.Text = "2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 112);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "region";
+            // 
+            // label_city
+            // 
+            this.label_city.AutoSize = true;
+            this.label_city.Location = new System.Drawing.Point(74, 81);
+            this.label_city.Name = "label_city";
+            this.label_city.Size = new System.Drawing.Size(13, 13);
+            this.label_city.TabIndex = 16;
+            this.label_city.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "city";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(209, 228);
+            this.label2.Location = new System.Drawing.Point(209, 233);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(286, 25);
             this.label2.TabIndex = 14;
@@ -88,7 +166,7 @@
             this.label_login.AutoSize = true;
             this.label_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label_login.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(143)))), ((int)(((byte)(202)))));
-            this.label_login.Location = new System.Drawing.Point(280, 175);
+            this.label_login.Location = new System.Drawing.Point(280, 180);
             this.label_login.Name = "label_login";
             this.label_login.Size = new System.Drawing.Size(165, 38);
             this.label_login.TabIndex = 11;
@@ -97,7 +175,7 @@
             // label_logo_login
             // 
             this.label_logo_login.Image = global::rainCheck.Properties.Resources.icon_32x32;
-            this.label_logo_login.Location = new System.Drawing.Point(232, 176);
+            this.label_logo_login.Location = new System.Drawing.Point(232, 181);
             this.label_logo_login.Name = "label_logo_login";
             this.label_logo_login.Size = new System.Drawing.Size(56, 39);
             this.label_logo_login.TabIndex = 12;
@@ -143,81 +221,67 @@
             this.pictureBox_loader.TabIndex = 0;
             this.pictureBox_loader.TabStop = false;
             // 
-            // panel_retry_authorisation
+            // panel_retry
             // 
-            this.panel_retry_authorisation.Controls.Add(this.button_retry_authorisation);
-            this.panel_retry_authorisation.Controls.Add(this.label_noconnection);
-            this.panel_retry_authorisation.Location = new System.Drawing.Point(12, 12);
-            this.panel_retry_authorisation.Name = "panel_retry_authorisation";
-            this.panel_retry_authorisation.Size = new System.Drawing.Size(680, 457);
-            this.panel_retry_authorisation.TabIndex = 15;
+            this.panel_retry.Controls.Add(this.button_retry);
+            this.panel_retry.Controls.Add(this.label1);
+            this.panel_retry.Location = new System.Drawing.Point(12, 12);
+            this.panel_retry.Name = "panel_retry";
+            this.panel_retry.Size = new System.Drawing.Size(680, 457);
+            this.panel_retry.TabIndex = 16;
             // 
-            // button_retry_authorisation
+            // button_retry
             // 
-            this.button_retry_authorisation.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(139)))), ((int)(((byte)(202)))));
-            this.button_retry_authorisation.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button_retry_authorisation.FlatAppearance.BorderSize = 0;
-            this.button_retry_authorisation.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button_retry_authorisation.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_retry_authorisation.ForeColor = System.Drawing.Color.White;
-            this.button_retry_authorisation.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button_retry_authorisation.Location = new System.Drawing.Point(283, 234);
-            this.button_retry_authorisation.Name = "button_retry_authorisation";
-            this.button_retry_authorisation.Size = new System.Drawing.Size(104, 29);
-            this.button_retry_authorisation.TabIndex = 11;
-            this.button_retry_authorisation.Text = "Retry";
-            this.button_retry_authorisation.UseVisualStyleBackColor = false;
-            this.button_retry_authorisation.Click += new System.EventHandler(this.button_retry_authorisation_Click);
-            // 
-            // label_noconnection
-            // 
-            this.label_noconnection.AutoSize = true;
-            this.label_noconnection.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_noconnection.ForeColor = System.Drawing.Color.White;
-            this.label_noconnection.Location = new System.Drawing.Point(213, 194);
-            this.label_noconnection.Name = "label_noconnection";
-            this.label_noconnection.Size = new System.Drawing.Size(250, 25);
-            this.label_noconnection.TabIndex = 2;
-            this.label_noconnection.Text = "No Internet connection";
-            this.label_noconnection.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // panel_retry_main
-            // 
-            this.panel_retry_main.Controls.Add(this.button_retry_main);
-            this.panel_retry_main.Controls.Add(this.label1);
-            this.panel_retry_main.Location = new System.Drawing.Point(12, 12);
-            this.panel_retry_main.Name = "panel_retry_main";
-            this.panel_retry_main.Size = new System.Drawing.Size(680, 457);
-            this.panel_retry_main.TabIndex = 16;
-            // 
-            // button_retry_main
-            // 
-            this.button_retry_main.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(139)))), ((int)(((byte)(202)))));
-            this.button_retry_main.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button_retry_main.FlatAppearance.BorderSize = 0;
-            this.button_retry_main.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button_retry_main.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_retry_main.ForeColor = System.Drawing.Color.White;
-            this.button_retry_main.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button_retry_main.Location = new System.Drawing.Point(283, 234);
-            this.button_retry_main.Name = "button_retry_main";
-            this.button_retry_main.Size = new System.Drawing.Size(104, 29);
-            this.button_retry_main.TabIndex = 11;
-            this.button_retry_main.Text = "Retry";
-            this.button_retry_main.UseVisualStyleBackColor = false;
-            this.button_retry_main.Click += new System.EventHandler(this.button_retry_main_Click);
+            this.button_retry.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(139)))), ((int)(((byte)(202)))));
+            this.button_retry.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_retry.FlatAppearance.BorderSize = 0;
+            this.button_retry.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_retry.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_retry.ForeColor = System.Drawing.Color.White;
+            this.button_retry.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button_retry.Location = new System.Drawing.Point(287, 234);
+            this.button_retry.Name = "button_retry";
+            this.button_retry.Size = new System.Drawing.Size(104, 29);
+            this.button_retry.TabIndex = 11;
+            this.button_retry.Text = "Retry (F5)";
+            this.button_retry.UseVisualStyleBackColor = false;
+            this.button_retry.Click += new System.EventHandler(this.button_retry_Click);
+            this.button_retry.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button_retry_KeyDown);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(213, 194);
+            this.label1.Location = new System.Drawing.Point(216, 194);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(250, 25);
             this.label1.TabIndex = 2;
             this.label1.Text = "No Internet connection";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 53);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "ip";
+            // 
+            // label_ip
+            // 
+            this.label_ip.AutoSize = true;
+            this.label_ip.Location = new System.Drawing.Point(74, 53);
+            this.label_ip.Name = "label_ip";
+            this.label_ip.Size = new System.Drawing.Size(13, 13);
+            this.label_ip.TabIndex = 24;
+            this.label_ip.Text = "1";
             // 
             // Form_Landing
             // 
@@ -225,10 +289,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(69)))), ((int)(((byte)(87)))));
             this.ClientSize = new System.Drawing.Size(704, 481);
-            this.Controls.Add(this.panel_retry_main);
-            this.Controls.Add(this.panel_retry_authorisation);
-            this.Controls.Add(this.panel_loader);
             this.Controls.Add(this.panel_authorization);
+            this.Controls.Add(this.panel_retry);
+            this.Controls.Add(this.panel_loader);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -245,10 +308,8 @@
             this.panel_loader.ResumeLayout(false);
             this.panel_loader.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_loader)).EndInit();
-            this.panel_retry_authorisation.ResumeLayout(false);
-            this.panel_retry_authorisation.PerformLayout();
-            this.panel_retry_main.ResumeLayout(false);
-            this.panel_retry_main.PerformLayout();
+            this.panel_retry.ResumeLayout(false);
+            this.panel_retry.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -260,14 +321,21 @@
         private System.Windows.Forms.Panel panel_loader;
         private System.Windows.Forms.PictureBox pictureBox_loader;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Panel panel_retry_authorisation;
-        private System.Windows.Forms.Label label_noconnection;
-        private System.Windows.Forms.Button button_retry_authorisation;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel panel_retry_main;
-        private System.Windows.Forms.Button button_retry_main;
+        private System.Windows.Forms.Panel panel_retry;
+        private System.Windows.Forms.Button button_retry;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label_city;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label_isp;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label_country;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label_region;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label_ip;
+        private System.Windows.Forms.Label label6;
     }
 }
--- rainCheck/rainCheck/Form_Landing.cs
+++ rainCheck/rainCheck/Form_Landing.cs
@@ -1,26 +1,38 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Globalization;
+﻿using System;
+using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Net.NetworkInformation;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace rainCheck
 {
     public partial class Form_Landing : Form
     {
-
-        Timer timer_authorisation = new Timer();
-        Timer timer_main = new Timer();
-
         public Form_Landing()
         {
             InitializeComponent();
         }
 
+        // Form load
         private void Form_Landing_Load(object sender, EventArgs e)
         {
-            goToPanelAuthorisation();
+            label_ip.Text = getExternalIp();
+        }
+
+        // Get external IP
+        private string getExternalIp()
+        {
+            try
+            {
+                string externalIP;
+                externalIP = (new WebClient()).DownloadString("https://canihazip.com/s");
+                externalIP = (new Regex(@"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"))
+                             .Matches(externalIP)[0].ToString();
+                return externalIP;
+            }
+            catch { return null; }
         }
 
         // Checking if connected to internet
@@ -39,7 +51,7 @@ namespace rainCheck
                 return false;
             }
         }
-                
+
         // Get MAC Address
         public void GetMACAddress()
         {
@@ -56,144 +68,47 @@ namespace rainCheck
             //label_mac_address.Text = sMacAddress;
         }
 
-        int i_authorisation = 0;
-        private void goToPanelAuthorisation() {
-            panel_loader.BringToFront();
-            timer_authorisation.Enabled = true;
-            timer_authorisation.Interval = 500;
-            timer_authorisation.Tick += (t, args) =>
-            {
-                i_authorisation++;
-                label4.Text = i_authorisation.ToString();
-
-                if (i_authorisation == 4)
-                {
-                    timer_authorisation.Stop();
-                    if (CheckForInternetConnection())
-                    {
-                        panel_authorization.BringToFront();
-                    }
-                    else
-                    {
-                        panel_retry_authorisation.BringToFront();
-                    }
-                }
-            };
-        }
-
-        int i_main = 0;
-        private void goToPanelMain()
+        int i = 0;
+        private void timer_Tick(object sender, EventArgs e)
         {
-            if (CheckForInternetConnection())
-            {
-                Form_Main form_main = new Form_Main();
-                this.Hide();
-                form_main.ShowDialog();
-                this.Close();
-            }
-            else
-            {
-                panel_loader.BringToFront();
-                timer_main.Interval = 500;
-                timer_main.Tick += (t, args) =>
-                {
-                    i_main++;
-                    label4.Text = i_main.ToString();
+            panel_loader.BringToFront();
+            i++;
 
-                    if (i_main == 4)
-                    {
-                        timer_main.Stop();
-                        if (CheckForInternetConnection())
-                        {
-                            Form_Main form_main = new Form_Main();
-                            this.Hide();
-                            form_main.ShowDialog();
-                            this.Close();
-                        }
-                        else
-                        {
-                            panel_retry_main.BringToFront();
-                        }
-                    }
-                };
-                timer_main.Enabled = true;
+            label4.Text = i.ToString();
+
+            if (i >= 20)
+            {
+                timer.Stop();
+                if (CheckForInternetConnection())
+                {
+                    panel_authorization.BringToFront();
+                    button_retry.Enabled = false;
+                }
+                else
+                {
+                    panel_retry.BringToFront();
+                    button_retry.Enabled = true;
+                }
             }
         }
 
         // Button retry
-        private void button_retry_authorisation_Click(object sender, EventArgs e)
+        private void button_retry_Click(object sender, EventArgs e)
         {
-            panel_loader.BringToFront();
-            timer_authorisation.Start();
-            timer_authorisation.Interval = 500;
-            timer_authorisation.Tick += (t, args) =>
-            {
-                i_authorisation++;
+            i = 0;
+            timer.Start();
+        }
 
-                if (i_authorisation >= 4)
+        private void button_retry_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F5)
+            {
+                if(i >= 20)
                 {
-                    timer_authorisation.Stop();
-                    if (CheckForInternetConnection())
-                    {
-                        panel_authorization.BringToFront();
-                    }
-                    else
-                    {
-                        panel_retry_authorisation.BringToFront();
-                    }
+                    i = 0;
+                    timer.Start();
                 }
-            };
-        }
-
-        private void button_retry_main_Click(object sender, EventArgs e)
-        {
-            panel_loader.BringToFront();
-            timer_main.Start();
-            timer_main.Interval = 500;
-            timer_main.Tick += (t, args) =>
-            {
-                i_main++;
-
-                if (i_main >= 4)
-                {
-                    timer_main.Stop();
-                    if (CheckForInternetConnection())
-                    {
-                        Form_Main form_main = new Form_Main();
-                        this.Hide();
-                        form_main.ShowDialog();
-                        this.Close();
-                    }
-                    else
-                    {
-                        panel_retry_main.BringToFront();
-                    }
-                }
-            };
-        }
-
-        // Country
-        public static string GetUserCountryByIp(string ip)
-        {
-            IpInfo ipInfo = new IpInfo();
-            try
-            {
-                string info = new WebClient().DownloadString("http://ipinfo.io/" + ip);
-                ipInfo = JsonConvert.DeserializeObject<IpInfo>(info);
-                RegionInfo myRI1 = new RegionInfo(ipInfo.Country);
-                ipInfo.Country = myRI1.EnglishName;
             }
-            catch (Exception)
-            {
-                ipInfo.Country = null;
-            }
-
-            return ipInfo.Country;
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            goToPanelMain();
         }
     }
 }
